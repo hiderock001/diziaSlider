7bc71d8abca956518673dda22549894b782cd3ce
unknown <hiderock@bookcube.co.kr>
2018-05-09 10:31:47 +0900

    renewal slider

--- js/diziaSlider.js
+++ js/diziaSlider.js
@@ -9,11 +9,12 @@
 				type  : 'slide', // slide, fade
 				speed : 5000,
 				delay : 1000,
+				auto		: true,
 			    controls	: true,		
-				prevText	: 'prev',	
-				nextText	: 'next',	
+				prevText	: '이전',	
+				nextText	: '다음',	
 				pager		: true,		
-				auto		: true
+				autoControls: true
 			},options);
 				
 			var _that = $(this);
@@ -22,6 +23,7 @@
 			var li = null;
 			var len = null;
 			var width = null;
+			var autoControl = true;
 			var first = _that.find("li:first");
 			var last = _that.find("li:last");
 			var timer = 0;
@@ -56,7 +58,10 @@
 					}
 					if(options.type=="slide"){
 						last.clone().prependTo(_that);
-						first.clone().appendTo(_that);	
+						first.clone().appendTo(_that);
+						_that.find("li a").attr("tabIndex",0);
+						_that.find("li:last a").attr("tabIndex",-1);
+						_that.find("li:first a").attr("tabIndex",-1);
 					}
 					options.type=="slide" ? _that.css({"width":width,"left": - li}) : _that.css({"width":"100%"})
 					
@@ -67,24 +72,32 @@
 					}
 
 					if(options.controls){
-						$(".diziaSlider-wrap").append("<div class='btn'><a href='javascript:;' class='dizia-prev'>"+options.prevText+"</a><a href='javascript:;' class='dizia-next'>"+options.nextText+"</a></div>");
+						$(".diziaSlider-wrap").prepend("<div class='dizia-btn'><a href='javascript:;' class='dizia-prev'>"+options.prevText+"</a><a href='javascript:;' class='dizia-next'>"+options.nextText+"</a></div>");
 					}
 					if(options.pager){
-						var anchor = "<a href='javascript:;'></a>";
+						var anchor =""; "<a href='javascript:;'></a>";
 						$(".diziaSlider-wrap").append("<div class='paging'></div>");
-						for(var i=0; i<len;i++){
+						for(var i=1; i<=len;i++){
+							anchor = "<a href='javascript:;'>"+i+"</a>";
 							$(".paging").append(anchor);
 						}
 						var an = wrapping.find(".paging a");
 						an.first().addClass("on");
 					}
 					
+
+					if(options.autoControls){
+						$(".dizia-btn").append("<a href='javascript:;' class='dizia-control on'>stop</a>");
+					}
+
 					var prevBtn = wrapping.find(".dizia-prev"),
-						nextBtn = wrapping.find(".dizia-next")
-					slider.controls(prevBtn,nextBtn,anchor);
+						nextBtn = wrapping.find(".dizia-next"),
+						controlBtn = wrapping.find(".dizia-control")
+
+					slider.controls(prevBtn,nextBtn,controlBtn);
 					
 				},
-				controls : function(prevBtn,nextBtn){
+				controls : function(prevBtn,nextBtn,controlBtn){
 
 					function next(){
 						var delay = Math.abs(timer - $.now());
@@ -147,6 +160,38 @@
 						prev();
 					});
 
+					if(options.auto){
+						var slideInterval = setInterval(next,options.delay);
+						$(".diziaSlider-wrap").on("mouseover focusin",function(){
+							stop();	
+						});
+						$(".diziaSlider-wrap").on("mouseleave blur",function(){
+							play();	
+						});
+					}
+
+					function stop(){
+						clearInterval(slideInterval);
+					}
+
+					function play(){
+						slideInterval = setInterval(next,options.delay);
+					}
+
+					controlBtn.on("click",function(){
+						if(autoControl){
+							autoControl = false;
+							$(this).text("start").removeClass("on");
+							stop();
+						}else{
+							autoControl = true;
+							$(this).text("stop").addClass("on");
+							play();
+						}
+					});
+
+
+
 					$(".paging a").on("click",function(){
 						var idx = $(this).index();
 						count = idx;
@@ -161,12 +206,6 @@
 							_that.find("li").eq(count).stop().fadeIn(options.speed);
 						}
 					});
-
-					if(options.auto){
-						setInterval(function(){
-							next();
-						},options.delay)
-					}
 
 				}
 			}
